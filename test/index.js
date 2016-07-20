42ca5aa5825475e5af129c5c488885501cf2628a
Prabhu Subramanian <prabhu@colearnr.com>
2016-07-20 01:53:25 +0100

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,70 @@
+const ExtractLib = require('../index')
+const vows = require('vows')
+const assert = require('assert')
+
+const suite = vows.describe('extract app test suite')
+const ExtractApp = new ExtractLib()
+
+suite.addBatch({
+  'Check if we can create the app': {
+    topic: function () {
+      this.callback(null, ExtractApp)
+    },
+    'check': function (err, obj) {
+      assert.equal(err, null)
+      assert.ok(obj)
+    }
+  },
+
+  'Check if we can process learnbit': {
+    topic: function () {
+      ExtractApp.processLearnbit(null, {url: 'http://www.bbc.co.uk/news/uk-36833042'}, this.callback)
+    },
+    'check': function (err, obj) {
+      assert.equal(err, null)
+      assert.ok(obj)
+      assert.equal(obj.publisher, 'BBC News')
+      assert.ok(obj.text)
+      assert.ok(obj.title)
+    }
+  },
+
+  'Check if we can parse bbc news': {
+    topic: function () {
+      ExtractApp.processUrl('http://www.bbc.co.uk/news/uk-36833042', this.callback)
+    },
+    'check': function (err, obj) {
+      assert.equal(err, null)
+      assert.ok(obj)
+      assert.equal(obj.publisher, 'BBC News')
+      assert.ok(obj.text)
+      assert.ok(obj.title)
+    }
+  },
+
+  'Check if we can parse wiki page': {
+    topic: function () {
+      ExtractApp.processUrl('https://en.wikipedia.org/wiki/Apple', this.callback)
+    },
+    'check': function (err, obj) {
+      assert.equal(err, null)
+      assert.ok(obj)
+      assert.equal(obj.lang, 'en')
+      assert.ok(obj.text)
+      assert.ok(obj.title)
+    }
+  },
+
+  'Check if we can process embed code': {
+    topic: function () {
+      ExtractApp.processEmbedCode(`<iframe width="420" height="315" src="https://www.youtube.com/embed/-Nc0wCrkk00" frameborder="0" allowfullscreen></iframe>`, this.callback)
+    },
+    'check': function (err, obj) {
+      assert.equal(err, null)
+      assert.ok(obj)
+      assert.equal(obj.lang, 'en')
+      assert.ok(obj.text)
+      assert.ok(obj.title)
+    }
+  }
+}).export(module)
